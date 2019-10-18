3916a644613271cddc75b393b5e20d6b386b1906
Your Name <you@example.com>
2019-10-18 07:54:55 -0300

    Hotfix no código

--- UDP/UDPServer.java
+++ UDP/UDPServer.java
@@ -9,8 +9,11 @@ class UDPServer {
 
       byte[ ] receiveData = new byte[1024]; // Formatação em Bytes, multiplataforma, para se padronizar os dados de envio/recebimento
       byte[ ] sendData  = new byte[1024];
-      serverSocket.connect(InetAddress.getByName("8.8.8.8"), 10002);
-      String ip = serverSocket.getLocalAddress().getHostAddress();
+
+      DatagramSocket serverSocketAux = new DatagramSocket(8080); //Cria uma porta no 8080
+
+      serverSocketAux.connect(InetAddress.getByName("8.8.8.8"), 10002);
+      String ip = serverSocketAux.getLocalAddress().getHostAddress();
 
       System.out.printf("Listening on %s:%d\n",ip,porta);
       while(true) // Servidores não desligam, ficam escutando eternamente
